449b1a84fe7896f3aa5a6bf7dfaf22d78f8057c1
Destiny <destinyerhen@gmail.com>
2024-12-22 03:04:41 +0100

    first commit

---
+++ data/awards.js
@@ -0,0 +1,23 @@
+export const awards = [
+  {
+    id: 1,
+    month: "June, 2022",
+    title: "Gold Medalist",
+    description: "Nigeria University Game",
+    imgSrc: "/assets/images/demo-bold/awards-3.jpg",
+  },
+  {
+    id: 2,
+    month: "May, 2020",
+    title: "Triple Gold Medalist",
+    description: "West-Africa University Game",
+    imgSrc: "/assets/images/demo-bold/awards-3.jpg",
+  },
+  {
+    id: 3,
+    month: "April, 2023",
+    title: "The Best ",
+    description: "National Championship",
+    imgSrc: "/assets/images/demo-bold/awards-3.jpg",
+  },
+];
